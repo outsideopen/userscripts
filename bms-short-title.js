4d52eed465863103e696dfa441738f78601c292b
Andy Rusch <arusch@outsideopen.com>
2020-11-18 15:27:59 -0800

    Updated title to remove Ticket number and added script for other pages. Updated Readme

--- bms-short-title.js
+++ bms-short-title.js
@@ -1,15 +1,23 @@
 // ==UserScript==
 // @name         BMS Remove title 'Outside Open - '
 // @namespace    https://github.com/outsideopen/userscripts
-// @version      0.2
-// @description  Remove's 'Outside Open - Ticket#' from HTML Title
+// @version      0.1
+// @description  Removes 'Outside Open - ' from HTML Title on non-ticket pages
 // @author       Andy Rusch <arusch@outsideopen.com>
-// @match        https://bms.kaseya.com/*
+// @match        https://bms.kaseya.com/MSP/MyTickets.aspx*
+// @match        https://bms.kaseya.com/Dashboard/TimesheetEdit.aspx*
+// @match        https://bms.kaseya.com/MSP/TicketsListing.aspx*
+// @match        https://bms.kaseya.com/CRM/*
+// @match        https://bms.kaseya.com/Finance/*
+// @match        https://bms.kaseya.com/Reports/*
+// @match        https://bms.kaseya.com/Projects/*
+// @match        https://bms.kaseya.com/Administration/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var newtitle = document.querySelector('.TicketTitle em span').innerText.replace(/^Ticket# /, '').replace(/\s+/ig,' ');    
+    var newtitle = document.title;
+    newtitle = newtitle.replace('Outside Open - ','');
     document.title = newtitle;
 })();
